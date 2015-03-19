6eb45a9fc370747629508700af33d89db3bf96cc
Caleb Hicks <calebhicks@gmail.com>
2015-03-19 10:38:08 -0600

    adds implementation for questioncontroller

--- ReviewDay/QuestionController.h
+++ ReviewDay/QuestionController.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Question.h"
+#import "User.h"
 
 @interface QuestionController : NSObject
+
+@property (strong, nonatomic, readonly) NSArray *questions;
+@property (strong, nonatomic, readonly) User *currentUser;
+
+- (Question *)createQuestionWithTitle:(NSString *)title details:(NSString *)details;
+
+- (void)removeQuestion:(Question *)question;
+
+- (void)save;
 
 @end
--- ReviewDay/QuestionController.m
+++ ReviewDay/QuestionController.m
@@ -7,7 +7,52 @@
 //
 
 #import "QuestionController.h"
+#import "Stack.h"
+
+typedef NS_ENUM(NSUInteger, QuestionStatus) {
+    StatusActive,
+    StatusAnswered,
+    StatusDeleted,
+};
 
 @implementation QuestionController
+
+- (NSArray *)questions {
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Question"];
+    
+    return [[Stack sharedInstance].managedObjectContext executeFetchRequest:request error:nil];
+}
+
+- (User *)currentUser {
+
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"User"];
+    
+    NSArray *users = [[Stack sharedInstance].managedObjectContext executeFetchRequest:request error:nil];
+    
+    return users.firstObject;
+}
+
+- (Question *)createQuestionWithTitle:(NSString *)title details:(NSString *)details {
+    Question *question = [NSEntityDescription insertNewObjectForEntityForName:@"Entry" inManagedObjectContext:[Stack sharedInstance].managedObjectContext];
+    
+    question.title = title;
+    question.details = details;
+    question.dateSubmitted = [NSDate date];
+    question.status = [NSNumber numberWithInteger:StatusActive];
+    question.submittedBy = [self currentUser];
+    
+    [self save];
+    
+    return question;
+}
+
+- (void)removeQuestion:(Question *)question {
+    [question.managedObjectContext deleteObject:question];
+    [self save];
+}
+
+- (void)save {
+    [[Stack sharedInstance].managedObjectContext save:nil];
+}
 
 @end
